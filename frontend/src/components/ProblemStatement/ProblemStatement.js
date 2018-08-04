d361fabea279fedff18e57d3b36ef4214daafb22
Joy Chen <joyhzchen@gmail.com>
2018-08-04 13:41:13 -0500

    Style problem statement some more

--- frontend/src/components/ProblemStatement/ProblemStatement.js
+++ frontend/src/components/ProblemStatement/ProblemStatement.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import { H2 } from "@blueprintjs/core";
+import { Card, H3, Icon } from "@blueprintjs/core";
 import './ProblemStatement.scss';
 
 export default class ProblemStatement extends Component {
   render() {
     return (
       <div className="problem-statement">
-        <H2>{this.props.problem}</H2>
+        <Card>
+            <H3>
+                <Icon icon="hand-right" iconSize={25} />
+                <span className="problem-urgent-label"> URGENT:</span>
+                <span className="problem-text"> {this.props.problem}</span>
+            </H3>
+        </Card>
       </div>
     );
   }
