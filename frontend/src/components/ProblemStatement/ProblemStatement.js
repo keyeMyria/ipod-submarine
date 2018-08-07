88a934d79092e72939b3db3e8fc803f5a30ec156
Joy Chen <joyhzchen@gmail.com>
2018-08-06 21:11:06 -0500

    don't display problem if user is alan

--- frontend/src/components/ProblemStatement/ProblemStatement.js
+++ frontend/src/components/ProblemStatement/ProblemStatement.js
@@ -4,14 +4,17 @@ import './ProblemStatement.scss';
 
 export default class ProblemStatement extends Component {
   render() {
+    const userIsAlan = this.props.alan === this.props.currentUser;
     return (
       <div className="problem-statement">
         <Card>
             <H3>
                 <Icon icon="hand-right" iconSize={25} />
                 <span className="problem-urgent-label"> URGENT:</span>
-                <span>{this.props.alan}</span>
-                <span className="problem-text"> {this.props.problem}</span>
+                {userIsAlan ? <span className="problem-text"> {this.props.problem}</span>
+                :
+                <span> You are Alan.</span>
+                }
             </H3>
         </Card>
       </div>
